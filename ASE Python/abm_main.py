6a822b2cd5d512e432e2968950f5a4a5980eb0ec
tsedl <tsedl@leeds.ac.uk>
2022-02-24 11:57:20 +0000

    better test data

--- ASE Python/abm_main.py
+++ ASE Python/abm_main.py
@@ -17,7 +17,7 @@ from Utilities import SingleThreadPubSub, GraphAStar
 ##                                  
 ##                              }"
 import yaml
-network_file_content = dct = yaml.safe_load('''
+example_content = dct = yaml.safe_load('''
 name: John
 age: 30
 automobiles:
@@ -28,4 +28,31 @@ automobiles:
   type: Sienna
   year: 2015
 ''')
+
+network_file_content = dct = yaml.safe_load('''
+nodes:
+- N1:[0.0, 0.0]
+- N2:[500.0, 0.0]
+- N3:[1000.0, 0.0]}
+links:
+- L1:
+- - start_id:N1
+- - end_id:N2
+- - waypoints:[[0.0, 0.0],
+               [250.0, 250.0],
+               [500.0, 0.0]]
+- L2:
+- - start_id:N1
+- - end_id:N2
+- - waypoints':[[0.0, 0.0],
+                [250.0, -250.0],
+                [500.0, 0.0]]              
+- L3:
+- - start_id:N2
+- - end_id:N3
+- - waypoints':[[500.0, 0.0],
+                [750.0, 0.0],
+                [1000.0, 0.0]]
+                                                              
+                                          ''')
 print(network_file_content)
