3084a4a743c19624b037c87f1f90b45a6aa4051d
tsedl <tsedl@leeds.ac.uk>
2022-02-25 11:01:08 +0000

    nearly create graph

--- ASE Python/abm_main.py
+++ ASE Python/abm_main.py
@@ -17,6 +17,7 @@ from Utilities import SingleThreadPubSub, GraphAStar
 ##                                  
 ##                              }"
 import yaml
+import numpy
 example_content = dct = yaml.safe_load('''
 name: John
 age: 30
@@ -44,15 +45,31 @@ links:
 - L2:
 - - start_id:N1
 - - end_id:N2
-- - waypoints':[[0.0, 0.0],
-                [250.0, -250.0],
-                [500.0, 0.0]]              
+- - waypoints:[[0.0, 0.0],
+               [250.0, -250.0],
+               [500.0, 0.0]]              
 - L3:
 - - start_id:N2
 - - end_id:N3
-- - waypoints':[[500.0, 0.0],
-                [750.0, 0.0],
-                [1000.0, 0.0]]
+- - waypoints:[[500.0, 0.0],
+               [750.0, 0.0],
+               [1000.0, 0.0]]
                                                               
                                           ''') 
 print(network_file_content)
+
+nodes = network_file_content['nodes']
+links = network_file_content['links']
+g = GraphAStar.Graph()
+for node_str in nodes:
+    str_list = node_str.split(':')
+    node_id = str_list[0]
+    pos_str = str_list[1]
+    num_str_arr = pos_str.replace('[', '').replace(']', '').split(',')
+    nnum_str_arr = numpy.array(num_str_arr)
+    pos = nnum_str_arr.astype(float)
+    g.AddNodeG(pos, [])
+pass #endfor
+
+
+    
